ccaf77da3a69b93641424a1de1bf458b66ba5a34
carlosviana <viana.mail@gmail.com>
2018-12-24 08:55:51 -0200

    ambiente preparado

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import {
+  Platform, StyleSheet, Text, View,
+} from 'react-native';
+
+import './config/ReactotronConfig';
+import './config/DevToolsConfig';
+
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
