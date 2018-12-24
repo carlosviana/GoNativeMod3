337947a61a88ff5f150210cbd0d4c7a16f7eff2e
carlosviana <viana.mail@gmail.com>
2018-12-24 08:58:52 -0200

    acerto index

--- src/index.js
+++ src/index.js
@@ -1,28 +1,23 @@
 import React, { Component } from 'react';
-import {
-  Platform, StyleSheet, Text, View,
-} from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 import './config/ReactotronConfig';
 import './config/DevToolsConfig';
 
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
-}
-
+const regular = '#F5FCFF';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: regular,
   },
 });
+
+export default class App extends Component {
+  state = {};
+
+  render() {
+    return <View style={styles.container} />;
+  }
+}
